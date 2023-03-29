9a89fa46a6f75faac167ef4e99bd679ce82fec49
Jumavictor <victorjuma48@gmail.com>
2023-03-29 01:51:44 -0700

    Task 21. a function that checks if a binary tree is complete

--- binary_trees.h
+++ binary_trees.h
@@ -100,6 +100,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 
+/* 101-binary_tree_levelorder.c */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
 /* 102-binary_tree_is_complete.c */
 int binary_tree_is_complete(const binary_tree_t *tree);
 
