176bb8704d07b33101ed5e16ec9be3500550001e
Jumavictor <victorjuma48@gmail.com>
2023-03-29 22:14:39 -0700

    Task 28.  a function that removes a node from a Binary Search Tree

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -7,36 +7,64 @@
  * Return: pointer to the new root node of the tree after removing the desired
  * value
  */
-typedef struct bst {
-    int value;
-    struct bst *left;
-    struct bst *right;
-} bst_t;
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *tmp;
 
-bst_t *bst_remove(bst_t *root, int value) {
-    if (root == NULL) {
-        return NULL;
-    }
-    if (value < root->value) {
-        root->left = bst_remove(root->left, value);
-    } else if (value > root->value) {
-        root->right = bst_remove(root->right, value);
-    } else {
-        if (root->left == NULL) {
-            bst_t *temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            bst_t *temp = root->left;
-            free(root);
-            return temp;
-        }
-        bst_t *temp = root->right;
-        while (temp->left != NULL) {
-            temp = temp->left;
-        }
-        root->value = temp->value;
-        root->right = bst_remove(root->right, temp->value);
-    }
-    return root;
+	if (!root)
+		return (NULL);
+	/* If the value is less than the root, */
+	if (value < root->n)
+		/* recursively call the function on the left side of the tree. */
+		root->left = bst_remove(root->left, value);
+	/* If the value is greater than the root, */
+	else if (value > root->n)
+		/* recursively call the function on the right side of the tree. */
+		root->right = bst_remove(root->right, value);
+	else /* If the value is equal to the root, we found the node to remove. */
+	{
+		if (!root->left) /* If the node to remove has no left child, */
+		{
+			/* then replace the node with its right child. */
+			tmp = root->right;
+			free(root);
+			return (tmp);
+		}
+		else if (!root->right) /* If the node to remove has no right child, */
+		{
+			tmp = root->left; /* then replace the node with its left child. */
+			free(root);
+			return (tmp);
+		}
+		/*
+		* If the node has both a left and right child then find the minimum
+		* value in the right subtree.
+		*/
+		tmp = bst_min_node(root->right);
+		/* replace the value of the node to remove with the minimum value. */
+		root->n = tmp->n;
+		/* recursively call the func on right subtree to remove min value node. */
+		root->right = bst_remove(root->right, tmp->n);
+	}
+	return (root); /* return the root */
+}
+
+/**
+ * bst_min_node - finds the minimum node in a binary search tree.
+ * @root: pointer to the root node of the tree to traverse.
+ *
+ * Return: pointer to the minimum node
+ */
+bst_t *bst_min_node(bst_t *root)
+{
+	bst_t *tmp;
+
+	/* Assign the address of the root node to the tmp pointer. */
+	tmp = root;
+
+	/* Loop until the left node of the tmp pointer is NULL. */
+	while (tmp->left != NULL)
+		tmp = tmp->left;
+	/* Return the address of the tmp pointer. */
+	return (tmp);
 }
